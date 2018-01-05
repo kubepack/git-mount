b235898967d8ef9cee38b2b4e3361d65aa145d8c
hanif <hanif@appscode.com>
2018-01-05 17:45:35 +0600

    Update

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,3 @@
-FROM scratch
+FROM appscode/kubectl:1.8.0
 ADD git-mount /bin/
 ENTRYPOINT ["git-mount"]
--- main.go
+++ main.go
@@ -12,14 +12,12 @@ import (
 
 func main()  {
 	for ; ; {
-		fmt.Println("Hello World!!!")
+		dicision := false
 		files, err := ioutil.ReadDir("/mypath")
 		if err != nil {
 			log.Println(err)
 		}
-		for key, value := range files {
-			fmt.Println("hello key----", key)
-			fmt.Println("hello value--", value)
+		for _, value := range files {
 			path := filepath.Join("/mypath", value.Name())
 			fileInfo, err := os.Stat(path)
 			if err != nil {
@@ -27,19 +25,30 @@ func main()  {
 			}
 			if fileInfo.IsDir() {
 				outPath := filepath.Join(path, "_outlook")
-				outFileInfo, err := os.Stat(outPath)
+				_, err := os.Stat(outPath)
 				if err == nil {
-					fmt.Println("-------------", outFileInfo)
-					cmd := exec.Command("kubectl", "apply", "-f", outPath)
+					cmd := exec.Command("kubectl", "apply", "-R" ,"-f", outPath)
 					cmd.Stdout = os.Stdout
 					cmd.Stderr = os.Stderr
+
+					dicision = true
 					if err = cmd.Run(); err != nil {
+						dicision = false
+						fmt.Println(err)
 						log.Println(err)
 					}
+
 					break
 				}
 			}
 		}
+		if dicision {
+			break
+		}
+	}
+
+	for ; ;  {
+		fmt.Println("")
 	}
 }
 
